63a9dccfe0285a2033341f533add32c7366746db
dawn_lch <chung4111@naver.com>
2024-03-09 18:09:58 +0900

    first

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        int n;
+        n=scan.nextInt();
     }
 }
